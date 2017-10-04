768954cbd0401a91cdf08719e41aa7b95c9d2ba4
Evgeniya Severinets <ekseverinets@gmail.com>
2017-10-04 18:31:41 +0300

    App creation, data display, and routing between views

--- models/models.js
+++ models/models.js
@@ -1,17 +1,17 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var postSchema = new mongoose.Schema({
+	created_by: String,		//should be changed to ObjectId, ref "User"
+	created_at: {type: Date, default: Date.now},
+	text: String
+});
+
 var userSchema = new mongoose.Schema({
 	username: String,
 	password: String, //hash created from password
 	created_at: {type: Date, default: Date.now}
 })
-
-var postSchema = new mongoose.Schema({
-	created_by: { type: Schema.ObjectId, ref: 'User' },		//should be changed to ObjectId, ref "User"
-	created_at: {type: Date, default: Date.now},
-	text: String
-});
 
 
 mongoose.model('Post', postSchema);
