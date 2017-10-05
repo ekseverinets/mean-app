61e44f0e45d3b07de8b60392d9ea6e41318de691
Evgeniya Severinets <ekseverinets@gmail.com>
2017-10-05 13:54:44 +0300

    Animation built with D3

--- public/javascripts/mainApp.js
+++ public/javascripts/mainApp.js
@@ -6,7 +6,36 @@ var app = angular.module('mainApp', ['ngRoute', 'ngResource']).run(function($roo
     	$http.get('auth/signout');
     	$rootScope.authenticated = false;
     	$rootScope.current_user = '';
-	};
+    };
+    
+    // D3 block
+    var i = 0;
+    
+    var svg = d3.select(".d3-block").append("svg");
+    
+    svg.append("rect")
+        .on("ontouchstart" in document ? "touchmove" : "mousemove", particle);
+    
+    function particle() {
+      var m = d3.mouse(this);
+    
+      svg.insert("circle", "rect")
+          .attr("cx", m[0])
+          .attr("cy", m[1])
+          .attr("r", 1e-6)
+          .style("stroke", d3.hsl((i = (i + 1) % 360), 1, .5))
+          .style("stroke-opacity", 1)
+        .transition()
+          .duration(2000)
+          .ease(Math.sqrt)
+          .attr("r", 100)
+          .style("stroke-opacity", 1e-6)
+          .remove();
+    
+      d3.event.preventDefault();
+    }
+    // End D3 block
+    
 });
 
 app.config(function($routeProvider){
